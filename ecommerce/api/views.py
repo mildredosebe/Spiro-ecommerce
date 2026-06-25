e43a78b448ea401b7c23544238667919f1d2ff31
mildredosebe <mildredosebeochoki@gmail.com>
2026-06-25 08:30:34 +0300

    added Authentication

--- ecommerce/api/views.py
+++ ecommerce/api/views.py
@@ -438,16 +438,11 @@ def scalar_docs(request):
     return HttpResponse(html)
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import LoginSerializer
-
-
-
 from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
-
-
 from django.contrib.auth import authenticate
 from rest_framework.views import APIView
 from rest_framework.response import Response
