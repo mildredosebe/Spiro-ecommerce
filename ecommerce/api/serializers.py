c87622b07576f06ef47015245df9f64c34474db3
mildredosebe <mildredosebeochoki@gmail.com>
2026-06-25 16:22:53 +0300

    updated

--- ecommerce/api/serializers.py
+++ ecommerce/api/serializers.py
@@ -98,7 +98,7 @@ class CartSerializer(serializers.ModelSerializer):
     class Meta:
         model = Cart
         fields = '__all__'
-        read_only_fields = ('user', 'cart_id')
+        read_only_fields = ('user')
 
 
 class CartItemSerializer(serializers.ModelSerializer):
